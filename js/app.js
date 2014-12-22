916e20914e1e84c50d561cc39755c0dce85876cf
IljaGoushcha <iljagou@yahoo.com>
2014-12-22 12:37:20 -0500

    added post with form

--- js/app.js
+++ js/app.js
@@ -3,22 +3,51 @@ angular.module('HttpPost', []);
 angular.module('HttpPost').controller('MainCtrl', function($scope, $http) {
     'use strict';
 
-    var songs = [];
+    $scope.songs = [];
 
     $scope.uploadSongNoForm = function() {
+      console.log("hi");
       var params = {
-        url: "http://www.song.com",
-        title: "Smells Like Teen Spirit",
-        author: "Nirvana",
-        album: "Nevermind",
-        pitch: 0,
-        volume: 0,
-        fade_start_time: 0,
-        fade_stop_time: 0
+        song : {
+          url: "http://www.song.com",
+          title: "Smells Like Teen Spirit",
+          author: "Nirvana",
+          album: "Nevermind",
+          pitch: 0,
+          volume: 0,
+          fade_start_time: 0,
+          fade_stop_time: 0,
+          controller: "songs",
+          action: "create"
+        }
       };
 
-      $http.post('http://localhost:3000/users', params).success(function(response) {
-            $scope.songs.push(response);
+      $http.post('http://localhost:3000/songs', params).success(function(response) {
+          console.log(response);
+          $scope.songs.push(response);
+      });
+    };
+
+    $scope.uploadSongWithForm = function() {
+      console.log("inside form");
+      var params = {
+        song : {
+          url: $scope.song.url,
+          title: $scope.song.title,
+          author: $scope.song.author,
+          album: "Nevermind",
+          pitch: 0,
+          volume: 0,
+          fade_start_time: 0,
+          fade_stop_time: 0,
+          controller: "songs",
+          action: "create"
+        }
+      };
+
+      $http.post('http://localhost:3000/songs', params).success(function(response) {
+          console.log(response);
+          $scope.songs.push(response);
       });
     };
 
